df0b18ea0564f36a0f2ab0ce9b56a70dad60073f
pamelaga-alt <pamelaga@umich.edu>
2026-08-03 01:19:59 -0400

    Implement new tracker button functionality
    Added functionality to start a new tracker and navigate between pages.

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ btnOldTracker.addEventListener('click', () => {
   console.log("User wants to continue an existing tracker");
   // Logic to load saved localStorage tracker will go here
 });
+// 1. Grab the HTML elements by their IDs
+const page1 = document.getElementById('page-1');
+const page2 = document.getElementById('page-2');
+const btnNewTracker = document.getElementById('btn-new-tracker');
+
+// 2. Add an event listener to the "Start New Tracker" button
+btnNewTracker.addEventListener('click', () => {
+  // Hide Page 1 by adding the 'hidden' class
+  page1.classList.add('hidden');
+  
+  // Show Page 2 by removing the 'hidden' class
+  page2.classList.remove('hidden');
+  
+  console.log("Navigated to Page 2!");
+});
